b55fa6dda19d1435ba4ed4767d8fe908faef629a
mrtexaznl <marco@debian-i7.(none)>
2014-03-05 01:56:51 +0100

    port for mediterraneancoin support

--- mediterraneancoin-button/script.js
+++ mediterraneancoin-button/script.js
@@ -1,23 +1,23 @@
 (function ($) {
     $(document).ready(function() {
-	$('.bitcoin-button').each(function () {
+	$('.mediterraneancoin-button').each(function () {
 	    var url = $(this).attr('href');
 	    var address = $(this).data('address');
-	    var div = $(this).wrap('<div class="bitcoin-div"/>').parent();
+	    var div = $(this).wrap('<div class="mediterraneancoin-div"/>').parent();
 	    var show_info = $(this).data('info');
 
-	    div.append('<a href="'+url+'" class="bitcoin-counter" style="display:none;">&nbsp;</a>');
-	    div.append('<div class="bitcoin-bubble" style="display:none;"><div><strong>Bitcoin address</strong></div><a href="'+url+'"><img src="http://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=H|0&chl='+encodeURIComponent(url)+'" width="200" height="200" alt="QR code"/></a><div><a href="'+url+'">'+address+'</a></div></div>');
+	    div.append('<a href="'+url+'" class="mediterraneancoin-counter" style="display:none;">&nbsp;</a>');
+	    div.append('<div class="mediterraneancoin-bubble" style="display:none;"><div><strong>Mediterraneancoin address</strong></div><a href="'+url+'"><img src="http://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=H|0&chl='+encodeURIComponent(url)+'" width="200" height="200" alt="QR code"/></a><div><a href="'+url+'">'+address+'</a></div></div>');
 
 	    div.hover(function () {
-		$(this).find('.bitcoin-bubble').fadeIn(150);
+		$(this).find('.mediterraneancoin-bubble').fadeIn(150);
 	    }, function () {
-		$(this).find('.bitcoin-bubble').fadeOut(150);
+		$(this).find('.mediterraneancoin-bubble').fadeOut(150);
 	    });
 
 	    if (show_info && show_info != 'none') {
-		$.get(bitcoin_button_ajax.url, { action: 'bitcoin-address-info', address: address }, function (data) {
-		    var counter = div.find('.bitcoin-counter');
+		$.get(mediterraneancoin_button_ajax.url, { action: 'mediterraneancoin-address-info', address: address }, function (data) {
+		    var counter = div.find('.mediterraneancoin-counter');
 		    if (show_info == 'transaction' && data.transactions !== undefined) {
 			counter.text(data.transactions+" tx");
 			counter.show();
